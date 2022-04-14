131d9a6a5b32b9841516b5bdc9c9fcc5efbe19b3
beckywan <beckywang04@gmail.com>
2022-04-14 17:03:08 -0400

    edited the agent form and date formatting

---
+++ week-09/field-agent-M09/field-agent-secured/src/main/java/learn/field_agent/security/SecurityConfig.java
@@ -0,0 +1,80 @@
+package learn.field_agent.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JwtConverter converter;
+
+    public SecurityConfig(JwtConverter converter) {
+        this.converter = converter;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+
+        http.cors();
+
+        http.authorizeRequests()
+                // TODO add antMatchers here to configure access to specific API endpoints
+                .antMatchers("/api/authenticate").permitAll()
+                .antMatchers("/api/create_account").permitAll()
+                .antMatchers("/refresh_token").authenticated()
+//                .antMatchers(HttpMethod.GET,
+//                        "/order").permitAll()
+                .antMatchers(HttpMethod.GET,
+                        "/api/agent", "/api/agent/*").permitAll()
+                .antMatchers(HttpMethod.POST,
+                        "/api/agent").hasAnyRole("USER", "ADMIN")
+                .antMatchers(HttpMethod.PUT,
+                        "/api/agent/*").hasAnyRole("USER", "ADMIN")
+                .antMatchers(HttpMethod.DELETE,
+                        "/api/agent/*").hasAnyRole("ADMIN")
+                .antMatchers("/**").denyAll()
+                // require authentication for any request...
+                .anyRequest().authenticated()
+                .and()
+                .addFilter(new JwtRequestFilter(authenticationManager(), converter))
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Override
+    @Bean
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder getEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("*");
+            }
+        };
+    }
+
+}
